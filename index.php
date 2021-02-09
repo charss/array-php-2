47933a33d7a079400b51340772c90e5b745b9af8
Charles Reyes <charleskylereyes16@gmail.com>
2021-02-09 18:31:39 +0800

    Finished program

--- index.php
+++ index.php
@@ -12,38 +12,35 @@
 					<div><label>1.</label><input class='grid-item' type=text name='num1' maxlength=9/></div>
 					<div><label>2.</label><input class='grid-item' type=text name='num2' maxlength=9/></div>
 					<div><label>3.</label><input class='grid-item' type=text name='num3' maxlength=9/></div>
-					<div><label>4.</label><input class='grid-item' type=text name='num4' maxlength=9/></div>
-					<div><label>5.</label><input class='grid-item' type=text name='num5' maxlength=9/></div>
-					<div><label>6.</label><input class='grid-item' type=text name='num6' maxlength=9/></div>
-					<div><label>7.</label><input class='grid-item' type=text name='num7' maxlength=9/></div>
-					<div><label>8.</label><input class='grid-item' type=text name='num8' maxlength=9/></div>
-					<div><label>9.</label><input class='grid-item' type=text name='num9' maxlength=9/></div>
-					<div><label>10. </label><input class='grid-item' type=text name='num10' id='ten'  maxlength=9/></div>
 
 				</div>
 				<input type='submit' name='submit'/>
 			</form>
 			
 			<?php
-				$arr = array();
-				$num_array = array();
-				$arr[] = 'Array List: ';
-
-				if (isset($_POST['submit'])) {
-					$arr[] = $_REQUEST['num1'];
-					$arr[] = $_REQUEST['num2'];
-					$arr[] = $_REQUEST['num3'];
-					$arr[] = $_REQUEST['num4'];
-					$arr[] = $_REQUEST['num5'];
-					$arr[] = $_REQUEST['num6'];
-					$arr[] = $_REQUEST['num7'];
-					$arr[] = $_REQUEST['num8'];
-					$arr[] = $_REQUEST['num9'];
-					$arr[] = $_REQUEST['num10'];
-
-					for ($i = 1; $i < count($arr); $i++) {
-						$num_array[] = (int)$arr[$i];
+				$second_run = False;
+				function operations($num1, $num2, $num3, $operation) {
+					if ($operation == 'add') {
+						return $num1 + $num2 + $num3;
+					} elseif ($operation == 'subs') {
+						return $num1 - $num2 - $num3;
+					} elseif ($operation == 'mult') {
+						return $num1 * $num2 * $num3;
+					} elseif ($operation == 'div') {
+						if ($num1 == 0 || $num2 == 0 || $num3 == 0) {
+							return "Error. You can't divide a number by 0.";
+						} else {
+							return $num1 / $num2 / $num3;
+						}
 					}
+				}
+				
+				$num1 = $num2 = $num3 = 0;
+				if (isset($_POST['submit'])) {
+					$num1 = $_REQUEST['num1'];
+					$num2 = $_REQUEST['num2'];
+					$num3 = $_REQUEST['num3'];
+					$second_run = True;
 				}
 			?>
 			
@@ -52,12 +49,10 @@
 				<tr>
 					<td colspan='2' class='array_list'>
 						<?php
-							for ($i = 0; $i < count($arr); $i++) {
-								if ($i == 0 || $i == count($arr) - 1) {
-									echo $arr[$i].' ';
-								} else {
-									echo $arr[$i].', ';
-								}
+							if ($second_run == False) {
+								echo ("My Parameter values: __, __, __");
+							} else {
+								echo ("My Parameter values: $num1, $num2, $num3");
 							}
 						?>
 					</td>
@@ -66,11 +61,7 @@
 					<td class='darken'>Addition:</td>
 					<td>
 						<?php
-							if (count($num_array) > 0) {
-								echo array_sum($num_array);
-							} else {
-								echo 0;
-							}
+							echo operations($num1, $num2, $num3, 'add');
 						?>
 					</td>
 				</tr>
@@ -78,15 +69,7 @@
 					<td class='darken'>Subtraction:</td>
 					<td>
 						<?php
-							$diff = 0;
-							if (count($num_array) > 0) {
-								for ($i = 0; $i < count($num_array); $i++) {
-									$diff -= $num_array[$i];
-								}
-								echo $diff;
-							} else {
-								echo 0;
-							}
+							echo operations($num1, $num2, $num3, 'subs');
 						?>
 					</td>
 				</tr>
@@ -94,11 +77,7 @@
 					<td class='darken'>Multiplication:</td>
 					<td>
 						<?php
-							if (count($num_array) > 0) {
-								echo array_product($num_array);
-							} else {
-								echo 0;
-							}
+							echo operations($num1, $num2, $num3, 'mult');
 						?>
 					</td>
 				</tr>
@@ -106,14 +85,10 @@
 					<td class='darken'>Division:</td>
 					<td>
 						<?php
-							$quo = 1;
-							if (count($num_array) > 0) {
-								for ($i = 0; $i < count($num_array); $i++) {
-									$quo /= $num_array[$i];
-								}
-								echo $quo;
-							} else {
+							if ($second_run == False) {
 								echo 0;
+							} else {
+								echo operations($num1, $num2, $num3, 'div');
 							}
 						?>
 					</td>
